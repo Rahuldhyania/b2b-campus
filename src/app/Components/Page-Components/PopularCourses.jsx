375c8bc2e32f6a449d26af3d6d7510b1a93ccab5
b2b <ankitbase2brand@gmail.com>
2025-12-03 20:32:16 +0530

    testing 03/12/25 new 20:32pm

--- src/app/Components/Page-Components/PopularCourses.jsx
+++ src/app/Components/Page-Components/PopularCourses.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Title from '../UiUx/Title'
 import Globaltitle from '../UiUx/Globaltitle';
 import CourseCard from './CourseCard';
-import titlewhitebg from "../../../../public/images/titlewhitebg.webp"
+import smallbublebg from "../../../../public/images/smallbublebg.webp";
 import Buttons from '../UiUx/Buttons';
 import ServicesSection from './ServiceCard';
 
@@ -38,26 +38,38 @@ export const Coursesbtn = [
 const PopularCourses = () => {
     return (
         <div className=' main-bg'>
-            <div className="relative py-12 flex justify-center items-center">
-
-                {/* Background Bubble Image */}
-                <img
-                    src={titlewhitebg.src}
-                    alt="bubble-bg"
-                    className="absolute top-1/2  left-1/2  -translate-x-1/2 -translate-y-1/2 w-[732px] md:w-[756px] sm:w-[300px] pointer-events-none select-none
-        "
+            <div className="skill-title-container bg-no-repeat   bg-bottom py-6"
+                style={{
+                    backgroundImage: `url(${smallbublebg.src}) `,
+                    backgroundSize: '732px auto'
+                }}
+            >
+                <Title
+                    title={'Popular  <sapn class="text-primary"> Courses </span>'}
+                    text_color={'text-secondary'}
                 />
+            </div>
+            <div className="w-full p-[25px]">
+                <div className="flex flex-wrap justify-center items-center gap-[10px]
+                  p-[11px] max-w-[1032px] m-auto mb-[3%] mt-[1%]
+                  border-none rounded-[30px] place-items-center bg-[#CEC5FF]">
 
-                {/* Title on top */}
-                <div className="relative z-10 text-center">
-                    <Title
-                        title={'Popular <span class="text-primary"> Courses </span>'}
-                        text_color={'text-secondary'}
-                    />
+                    {Coursesbtn.map((item) => (
+                        <div key={item.id} className="cursor-pointer">
+                            <p className="bg-[#E4DFFF] text-[16px] sm:text-[17px] md:text-[18px] lg:text-[19px] 
+    p-[16px] sm:p-[17px] md:p-[18px] coures-btn rounded-[20px] transition-all duration-300 ease-in-out 
+    hover:bg-[#6346FA] hover:text-white hover:scale-105 hover:shadow-lg">
+                                {item.btn}
+                            </p>
+                        </div>
+                    ))}
+
                 </div>
             </div>
 
+
             <ServicesSection />
+
             <div className="buttons text-[25px] flex justify-center m-auto sm:mb-0">
                 <Buttons btnname='Start Your Career Journey Today!' text_color={'text-white'} />
             </div>
